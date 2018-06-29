be9f7febb5a793f59d96d54410b4644546842d5a
Bruno Martins <brunocorream@gmail.com>
2018-06-29 00:57:33 -0300

    Atualização da barra de progresso para que ao acertar uma resposta, incremente a porcentagem.

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from './../shared/frase.model';
+
+export const FRASES: Frase[] = [
+  { fraseEng: 'I like to Learn', frasePtbr: 'Eu gosto de aprender' },
+  { fraseEng: 'I watch TV', frasePtbr: 'Eu assisto TV' },
+  { fraseEng: 'How are You', frasePtbr: 'Como vai você' },
+  { fraseEng: 'I eat bread', frasePtbr: 'Eu como pão' },
+];
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from './../shared/frase.model';
+import { FRASES } from './frases-mock';
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a frase abaixo se for capaz!';
+  public resposta: string = '';
+
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  public progresso: number = 0;
+
+  constructor() {
+    this.atualizaRodada();
+  }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    // console.log( (<HTMLInputElement>resposta.target).value);
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtbr === this.resposta) {
+      alert('OK');
+
+      // Trocar a pergunta da rodada
+      this.rodada++;
+
+      // Aumenta o progresso
+      this.progresso = this.progresso + (100 / this.frases.length);
+
+      // Atualiza a frase
+      this.atualizaRodada();
+
+
+
+    } else {
+      alert('Deu Ruim!');
+    }
+  }
+
+  public atualizaRodada(): void{
+    this.rodadaFrase = this.frases[this.rodada];
+    // Limpa a resposta
+    this.resposta = '';
+  }
 }
